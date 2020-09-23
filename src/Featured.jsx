e8963b9a9cd3b9dcb062f6b1ebcb6ea39e243520
thomas turner <thomastdt@gmail.com>
2020-09-22 17:59:52 -0700

    src/Featured.jsx: next line test
    Signed-off-by: thomas turner <thomastdt@gmail.com>

--- src/Featured.jsx
+++ src/Featured.jsx
@@ -6,7 +6,7 @@ const Featured = (props) => {
         mainTitle="Featured" 
         imgSrc="../images/featured.jpg"
         imgAlt="nike neon"
-        title="Bold And Bright"
+        title="Bold And Bright Next Line"
         para="Show off your confidence in neon."
         m="84px auto 0"
         maxHeight="700px"
